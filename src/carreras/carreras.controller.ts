fd33352b37612d7d75bea260ca446948c97febb0
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-07-02 16:06:00 -0600

    feat: Listar y crear carreras

--- src/carreras/carreras.controller.ts
+++ src/carreras/carreras.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CarrerasService } from './carreras.service';
+import { CrearCarreraDto } from './dto/crear-carrera.dto';
+import { ActualizarCarreraDto } from './dto/actualizar-carrera.dto';
 
-@Controller({})
+@Controller('carreras')
 export class CarrerasController {
-    @Get('/carreras')
-    obtenerCarreras() {
-        return 'Obteniendo todas las carreras';
+    constructor(private carrerasService: CarrerasService) {}
+
+    @Get()
+    obtenerCarreras(@Query() query: any) {
+        console.log(query);
+        return this.carrerasService.obtenerCarreras();
+    }
+
+    @Get('/:id')
+    obtenerCarrera(@Param('id') id: string) {
+        return this.carrerasService.obtenerCarrera(parseInt(id));
+    }
+
+    @Post()
+    crearCarrera(@Body() carrera: CrearCarreraDto) {
+        return this.carrerasService.crearCarrera(carrera);
+    }
+
+    @Put()
+    actualizarCarrera(@Body() carrera: ActualizarCarreraDto) {
+        return this.carrerasService.actualizarCarrera(carrera);
+    }
+
+    @Patch()
+    actualizarClaveCarrera() {
+        return this.carrerasService.actualizarClaveCarrera();
+    }
+
+    @Delete()
+    eliminarCarrera() {
+        return this.carrerasService.eliminarCarrera();
     }
 }
--- src/carreras/carreras.service.ts
+++ src/carreras/carreras.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CrearCarreraDto } from './dto/crear-carrera.dto';
+import { ActualizarCarreraDto } from './dto/actualizar-carrera.dto';
+
+export interface Carrera {
+    clave: string;
+    nombre: string;
+}
 
 @Injectable()
-export class CarrerasService {}
+export class CarrerasService {
+    private carreras = [];
+
+    obtenerCarreras() {
+        return this.carreras;
+    }
+
+    obtenerCarrera(id: number) {
+        const carrera = this.carreras.find(carrera => carrera.id === id);
+
+        if (!carrera) {
+            return new NotFoundException(`No se encontró la carrera con el id ${id}`);
+        }
+
+        return carrera;
+    }
+
+    crearCarrera(carrera: CrearCarreraDto) {
+        this.carreras.push({
+            ...carrera,
+            id: this.carreras.length + 1
+        });
+
+        return carrera;
+    }
+
+    actualizarCarrera(carrera: ActualizarCarreraDto) {
+        return 'Actualizando carreras';
+    }
+
+    actualizarClaveCarrera() {
+        return 'Actualizando la clave de una carrera';
+    }
+
+    eliminarCarrera() {
+        return 'Eliminando carreras';
+    }
+}
