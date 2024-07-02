0afa3e49701127027360b2a055c1bcf01e1f0be1
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-07-02 13:42:59 -0600

    feat: Registrar index controller en app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AlumnosModule } from './alumnos/alumnos.module';
 import { CarrerasModule } from './carreras/carreras.module';
+import { IndexController } from './index/index.controller';
 
 @Module({
   imports: [AlumnosModule, CarrerasModule],
-  controllers: [],
+  controllers: [IndexController],
   providers: [],
 })
 export class AppModule {}
