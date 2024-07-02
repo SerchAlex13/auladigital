620e8eddd4c8f578cff4d25da5734d006d303d85
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-07-02 12:12:37 -0600

    feat: Validaciones

--- src/alumnos/dto/alumno.dto.ts
+++ src/alumnos/dto/alumno.dto.ts
@@ -1,15 +1,35 @@
-import { EstatusAlumno } from "../alumno.entity"
+import { EstatusAlumno } from '../alumno.entity';
+import { IsEnum, IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CrearAlumnoDTO {
+    @IsString()
+    @IsNotEmpty()
     nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
     apellido_paterno: string;
+
+    @IsString()
+    @IsNotEmpty()
     apellido_materno: string;
-    estatus: EstatusAlumno;
 }
 
 export class ActualizarAlumnoDTO {
+    @IsString()
+    @IsOptional()
     nombre?: string;
+
+    @IsString()
+    @IsOptional()
     apellido_paterno?: string;
+
+    @IsString()
+    @IsOptional()
     apellido_materno?: string;
+
+    @IsString()
+    @IsEnum(EstatusAlumno)
+    @IsOptional()
     estatus?: EstatusAlumno;
 }
