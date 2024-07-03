ebf926ac70363733cc43205f8f3aa2520386cd03
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2024-07-03 08:58:36 -0600

    feat: Aplicar middlewares

--- src/carreras/carreras.module.ts
+++ src/carreras/carreras.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { CarrerasController } from './carreras.controller';
 import { CarrerasService } from './carreras.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   controllers: [CarrerasController],
   providers: [CarrerasService]
 })
-export class CarrerasModule {}
+export class CarrerasModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('carreras').apply(AuthMiddleware).forRoutes('carreras');
+
+    consumer.apply(LoggerMiddleware).forRoutes(
+      {path: '/carreras', method: RequestMethod.GET}
+    );
+  }
+}
